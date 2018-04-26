3cbbd0885c46a1dd362052319384cca41a7b8a6d
gauravASC <38764674+gauravASC@users.noreply.github.com>
2018-04-26 14:32:52 -0400

    Create pokemon.js

---
+++ pokemon.js
@@ -0,0 +1,44 @@
+class Pokemon { 
+    constructor(name, type, hp, def, atk, legend) {
+        if (typeof(name) !== "string")
+            throw new TypeError('Invalid name');
+        else if (typeof(type) !== "string")
+            throw new TypeError('Invalid type');
+        else if (typeof(hp) !== "number")
+            throw new TypeError('Invalid hp');
+        else if (typeof(def) !== "number")
+            throw new TypeError('Invalid def');
+        else if (typeof(atk) !== "number" )
+            throw new TypeError('Invalid atk');
+        else if (typeof(legend) !== "boolean")
+            throw new TypeError('Invalid legend');
+        else{
+            this.name = name;
+            this.type = type;
+            this.hp = hp;
+            this.def = def;
+            this.atk = atk;
+            this.legend = legend;
+            
+            Pokemon.all.push(this);
+        }
+    }
+    
+    attack(target){
+        if (target instanceof Pokemon){
+            if ( (this.atk - target.def) > 0 )
+                target.hp = target.hp - (this.atk - target.def);
+        }
+        else
+            throw new TypeError('Target is not a pokemon');
+    }
+
+    aliveCheck(){
+        if (this.hp > 0)
+            return true;
+        else
+            return false;
+    } 
+}
+
+Pokemon.all = [];
